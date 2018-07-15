76d63114475b1f0486c69b0220aec67453874231
Isaac Steiner <izakizak@gmail.com>
2018-07-15 19:45:00 -0400

    Created seeder

---
+++ PersonContactApp/ContactLibrary/Phone.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ContactLibrary
+{
+    public class Phone
+    {
+        public long Pid { get; }
+        Country countryCode { get; set; }
+        string number { get; set; }
+        string areaCode { get; set; }
+        string extension { get; set; }
+
+        // Storing and validating phone numbers per: https://stackoverflow.com/a/4729239/3561626
+        public Phone(string number, Country countryCode = Country.United_States, string areaCode = "", string extension = "")
+        {
+            // Remove all non-digits from input
+            number = CleanToDigits(number);
+            areaCode = CleanToDigits(areaCode);
+            extension = CleanToDigits(extension);
+
+            // Validate main phone number
+            if (string.IsNullOrWhiteSpace(number))
+            {
+                throw new ArgumentException("Number is required when creating a phone number.");
+            }
+
+            if (number.Length > 15)
+            {
+                throw new ArgumentException($"Phone number must not be longer than 15 digits. Received: {number}");
+            }
+
+            // Validate areaCode
+            if (areaCode != "" && areaCode.Length > 5)
+            {
+                throw new ArgumentException($"Area code must not be longer than 5 digits. Received: {areaCode}");
+            }
+
+            // Validate extension
+            if (extension != "" && extension.Length > 11)
+            {
+                throw new ArgumentException($"Extension must not be longer than 5 digits. Received: {extension}");
+            }
+
+            this.number = number;
+            this.areaCode = areaCode;
+            this.extension = extension;
+            this.countryCode = countryCode;
+        }
+
+        private static string CleanToDigits(string text)
+        {
+            Regex justDigits = new Regex(@"[^\d]");
+            return justDigits.Replace(text, "");
+        }
+    }
+}
---
+++ PersonContactApp/ContactLibrary/Seeders/AddressSeeder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactLibrary.Seeders
+{
+    public static class AddressSeeder
+    {
+        public static string SingleName => "Address";
+        public static string PluralName => "Addreses";
+
+        public static void seed(int seedCount = 1, bool usaOnly=false)
+        {
+            string title;
+            Country country;
+            State state;
+            string houseNum;
+            string street;
+            string city;
+            string zip;
+
+            Address tempAddress;
+            string[] titleOptions = { "Home", "Work", "Other", "" };
+            Array countries = Enum.GetValues(typeof(Country));
+            Array states = Enum.GetValues(typeof(State));
+
+            for (int i = 0; i < seedCount; i++)
+            {
+                Random rnd = new Random();
+
+                // Get a random title
+                title = titleOptions[rnd.Next(titleOptions.Length)];
+
+                // Random country, unless usaOnly is set to true
+                country = usaOnly ? Country.United_States : (Country)countries.GetValue(rnd.Next(countries.Length));
+
+                // Only get a state if the country is USA
+                if (country.Equals(Country.United_States) || usaOnly == true)
+                {
+                    state = (State)states.GetValue(rnd.Next(states.Length));
+                }
+                else
+                {
+                    state = State.NA;
+                }
+
+                houseNum = rnd.Next(9999).ToString();
+                // *Everyone* lives on Main Street
+                street = "Main Street";
+                city = "Springfield";
+                zip = rnd.Next(99999).ToString().PadLeft(5, '0');
+
+                // Finally create the actual address
+                tempAddress = new Address(country, houseNum, street, city, zip, state, title);
+
+                tempAddress.save();
+            }
+        }
+    }
+}
