3dc2e4d3f3cdbc683f79143ab0356e114c9b0837
Isaac Steiner <ilebwohlsteiner@gmail.com>
2018-07-16 15:03:31 -0400

    Initial state

---
+++ PersonContactApp/ContactLibrary/Seeders/ContactSeeder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactLibrary.Seeders
+{
+    public static class ContactSeeder
+    {
+        public static string SingleName => "Contact";
+        public static string PluralName => "Contacts";
+
+        public static List<Contact> seed(ContactDirectory contactDirectory, int seedCount = 1, bool usaOnly = false)
+        {
+            string firstName;
+            string lastName;
+            Address address;
+            Phone phone;
+            List<Contact> contacts = new List<Contact>();
+
+            for(int i = 0; i < seedCount; i++)
+            {
+                firstName = "John";
+                lastName = "Smith";
+                address = AddressSeeder.seed(1)[0];
+                phone = new Phone("123456789", Country.United_States, "1234", "1234");
+
+                Contact contact = new Contact(firstName, lastName, phone, address);
+
+                contacts.Add(contact);
+            }
+            return contacts;
+    }
+    }
+}
