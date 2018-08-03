aaaf3eafd9b1fb33cbd61e6292e73242ba567825
Isaac Steiner <ilebwohlsteiner@gmail.com>
2018-08-03 18:38:10 -0400

    Created country and state controllers

---
+++ RoboDexApi/Controllers/CountryController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using PhoneDirectoryLibrary;
+
+namespace RoboDexApi.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class CountryController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get(string countryLookup = "")
+        {
+            Regex regex = new Regex("[0-9]+");
+
+            // Get all the countries
+            if (countryLookup.Length == 0)
+            {
+                return Json(Lookups.CountryKeys());
+            }
+            // Lookup by country code
+            else if (regex.IsMatch(countryLookup))
+            {
+                return Json<string>(Lookups.CountryKeys()[Convert.ToInt32(countryLookup)]);
+            }
+            else
+            {
+                return Json<int>((int)(Country)Enum.Parse(typeof(Country), countryLookup));
+            }
+        }
+    }
+}
---
+++ RoboDexApi/Controllers/StateController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using PhoneDirectoryLibrary;
+
+namespace RoboDexApi.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class StateController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get(string stateLookup = "")
+        {
+            try
+            {
+                Regex regex = new Regex("[0-9]+");
+
+                // Get all states
+                if (stateLookup.Length == 0)
+                {
+                    return Json<Dictionary<State, string>>(Lookups.StateNames);
+                }
+                // Lookup by numeric code
+                else if (regex.IsMatch(stateLookup))
+                {
+                    return Json<KeyValuePair<string, string>>(
+                        new KeyValuePair<string, string>(
+                            Enum.Parse(typeof(State), stateLookup).ToString(),
+                            Lookups.StateNames[Lookups.GetStateByCode(
+                                Enum.Parse(typeof(State), stateLookup).ToString())]
+                                ));
+                }
+                // Lookup by two-letter code
+                else if (stateLookup.Length == 2)
+                {
+                    return Json<State>(Lookups.GetStateByCode(stateLookup));
+                }
+                // Lookup by name
+                else
+                {
+                    return Json<State>(Lookups.GetStateByName(stateLookup));
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
